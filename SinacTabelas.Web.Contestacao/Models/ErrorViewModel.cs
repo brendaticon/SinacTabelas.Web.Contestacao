3ea27d1c128b2d05566e7c81197dd5c31ff09629
brendaticon@gmail.com <brendaticon@gmail.com>
2019-10-22 15:10:55 -0300

    primeiro teste

---
+++ SinacTabelas.Web.Contestacao/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SinacTabelas.Web.Contestacao.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
